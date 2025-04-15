a17af6a18af38b5691960f3b10ac971d6481276c
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-04-15 11:44:16 +0530

    chore: update default tw utilities

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -39,14 +39,19 @@ export default function Index() {
                 href={`#${item}`}
                 className={cn(
                   isActive ? 'text-foreground' : 'text-neutral',
-                  'flex items-center gap-2.5 text-[0.75em] font-bold tracking-widest uppercase'
+                  'group hover:text-foreground inline-flex w-max items-center gap-2.5 text-[0.75em] font-bold tracking-widest uppercase transition-all'
                 )}
               >
                 <span className="tracking-wide">
                   <span className="text-primary">0</span>
                   {idx + 1}
                 </span>
-                <span className={cn(isActive ? 'bg-foreground w-10' : 'bg-neutral w-5', 'h-px')}></span>
+                <span
+                  className={cn(
+                    isActive ? 'bg-foreground w-10' : 'bg-neutral w-5',
+                    'group-hover:bg-foreground h-px transition-all group-hover:w-10'
+                  )}
+                ></span>
                 {item}
               </a>
             );
