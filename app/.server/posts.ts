d8d58be96d6a99eb0085c81fee3dfdaddfdf1e80
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-04-19 07:47:04 +0530

    refactor: posts fetch error handle

--- app/.server/posts.ts
+++ app/.server/posts.ts
@@ -1,7 +1,16 @@
 import { Post } from '~/interfaces/post';
 
 export async function getAllPosts(): Promise<Post[]> {
-  const response = await fetch('https://moonlitgrace.space/api/blog');
-  const data = await response.json();
-  return data.data;
+  try {
+    const response = await fetch(process.env.BLOG_API_URL as string);
+    if (!response.ok) {
+      throw new Error(`Error, status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.data;
+  } catch (err) {
+    console.error('Failed to fetch posts: ', err);
+    return [];
+  }
 }
